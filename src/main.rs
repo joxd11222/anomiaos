993ca3e83ff8a853076c08c0cb4807950f607bf6
joxd11222 <157251766+joxd11222@users.noreply.github.com>
2025-09-27 00:37:08 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,34 @@ use core::panic::PanicInfo;
 mod vga_buffer;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    let mut writer = vga_buffer::Writer {
+        row_position: 0,
+        column_position: 0,
+        color_code: vga_buffer::ColorCode::new(
+            vga_buffer::Color::Red,
+            vga_buffer::Color::Black,
+        ),
+        buffer: unsafe { &mut *(0xb8000 as *mut vga_buffer::Buffer) },
+    };
+    writer.clear_screen();
+    writer.write_string("!!! PANIC !!!\n");
+    if let Some(location) = info.location() {
+        writer.write_string("panic at ");
+        writer.write_string(location.file());
+        writer.write_string(":");
+        let mut buf = [0u8; 20];
+        writer.write_string(&vga_buffer::int_to_string(location.line() as usize, &mut buf));
+        writer.write_string("\n");
+    } else {
+        writer.write_string("panic location unknown.\n");
+    }
+    let msg = info.message();
+    writer.write_string("panic message: ");
+    writer.write_string(msg.as_str().unwrap_or("no know message bruv"));
+    writer.write_string("\n");
     loop {}
 }
-
-static HELLO: &[u8] = b"Welcome to Anomia OS!";
 
 #[unsafe(no_mangle)] 
 pub extern "C" fn _start() -> ! {
@@ -23,7 +46,9 @@ pub extern "C" fn _start() -> ! {
         buffer: unsafe { &mut *(0xb8000 as *mut vga_buffer::Buffer) },
     };
     writer.clear_screen();
-    writer.write_string(core::str::from_utf8(HELLO).unwrap());
+    writer.color_code = vga_buffer::ColorCode::new(vga_buffer::Color::LightCyan, vga_buffer::Color::Black);
+    writer.write_string("==== WELCOME TO ANOMIA OS ====\n");
+    writer.color_code = vga_buffer::ColorCode::new(vga_buffer::Color::White, vga_buffer::Color::Black);
     writer.write_string("\n");
     writer.write_string("different tests running, wait for it...\n");
     writer.write_string("testing the scrolling function\n");
@@ -38,6 +63,20 @@ pub extern "C" fn _start() -> ! {
     writer.write_string("assertion test: ");
     assert_eq!(1, 1);
     writer.write_string("it works!\n");
+    writer.write_string("color test:\n");
+    vga_buffer::color_test();
+    writer.write_string("it works!\n");
+    writer.write_string("ascii test:\n");
+    vga_buffer::ascii_test();
+    writer.write_string("it works!\n");
+    writer.write_string("math test:\n");
+    vga_buffer::math_test();
+    writer.write_string("it works!\n");
+    writer.write_string("keyboard test:\n");
+    vga_buffer::keyboard_test();
+    writer.write_string("it works!\n");
+    writer.write_string("panic test:\n");
+    vga_buffer::panic_test();
     writer.write_string("all tests passed!\n");
     writer.write_string("Press Enter to exit...\n");
     vga_buffer::wait_for_enter();
